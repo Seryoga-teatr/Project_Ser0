7ffabefa9d8df9a336eb0c04317f3d44a60736d4
Sergey Lyashev <ser59617710@mail.ru>
2025-01-04 08:49:27 +0300

    add widget.py-get_date func

--- src/widget.py
+++ src/widget.py
@@ -7,15 +7,17 @@ def mask_account_card(name_of_card: str) -> str:
     list_in_func = name_of_card.split(' ')
     if list_in_func[0] == 'Счет':
         list_in_func[-1] = get_mask_account(int(list_in_func[-1]))
-#        mask_of_name = ' '.join(list_in_func)
-#        mask_of_name = "Счет " + get_mask_account(int(list_in_func[1]))
     else:
         list_in_func[-1] = get_mask_card_number(int(list_in_func[-1]))
-#        mask_of_name = ' '.join(list_in_func)
     return ' '.join(list_in_func)
 
 
+def get_date(date_time_str: str) -> str:
+    '''Вывод даты в формате: ДД.ММ.ГГГГ'''
+    #"2024-03-11T02:26:18.671407" --- "11.03.2024"
+    return date_time_str[8:10] + '.' + date_time_str[5:7] + '.' + date_time_str[0:4]
+
 if __name__ == "__main__":
-#    print(get_mask_card_number(1564487911112386))
     print(mask_account_card('Visa Gold 5999414228426353'))
     print(mask_account_card('Счет 73654108430135874305'))
+    print(get_date('2024-03-11T02:26:18.671407'))
